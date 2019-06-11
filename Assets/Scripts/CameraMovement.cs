591cf5137e3e732c82e6b37aaa47048b33dbd6b3
Jo-Jo Hupp <huppjoesaphean@gmail.com>
2019-06-11 07:32:17 -0700

    Added in basic scripting. Can move the ball and have camera follow it. 6/11/2019

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour{
+    public GameObject tempPlayer;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - tempPlayer.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = tempPlayer.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/Playercontroller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playercontroller : MonoBehaviour {
+    private Rigidbody rb;
+
+    public float speed; //Puts the variable in the editor to easily manipulate
+
+    void Update() { }
+
+    void Start(){ //Before anything is called, checks the player object for any compnents listed below
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate(){
+        float moveHorizontal = Input.GetAxis("Horizontal"); //Gets the horizontal change from the keyboard
+        float moveVertical = Input.GetAxis("Vertical"); //Gets the vertical change from the keyboard
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical); //Vector3 needs to have three variable, xyz
+
+        rb.AddForce(movement * speed); //Changes where the player object is going based on the Vector3 variables
+    }
+}
