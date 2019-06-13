d6d358fa0ee471a592e6676751cf69156bdc8491
Jo-Jo Hupp <huppjoesaphean@gmail.com>
2019-06-13 08:18:36 -0700

    Added some materials and scripts 6/13/2019

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour{
     public GameObject tempPlayer;
-
+    public float speed;
     private Vector3 offset;
 
     void Start()
     {
         offset = transform.position - tempPlayer.transform.position;
+        
     }
 
     void LateUpdate()
     {
+        //Camera mycam = GetComponent<Camera>();
         transform.position = tempPlayer.transform.position + offset;
+        //transform.LookAt(mycam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, mycam.nearClipPlane)), Vector3.up);
+        if (Input.GetKey(KeyCode.UpArrow))
+            transform.Rotate(-Vector3.right * speed * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.DownArrow))
+            transform.Rotate(Vector3.right * speed * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(Vector3.up * speed * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(-Vector3.up * speed * Time.deltaTime);
+
+       
     }
 }
--- Assets/Scripts/Playercontroller.cs
+++ Assets/Scripts/Playercontroller.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Playercontroller : MonoBehaviour {
     private Rigidbody rb;
-
+    private float NULL = 1;
     public float speed; //Puts the variable in the editor to easily manipulate
-
+    public float jump;
     void Update() { }
 
     void Start(){ //Before anything is called, checks the player object for any compnents listed below
@@ -14,11 +14,34 @@ public class Playercontroller : MonoBehaviour {
     }
 
     void FixedUpdate(){
-        float moveHorizontal = Input.GetAxis("Horizontal"); //Gets the horizontal change from the keyboard
-        float moveVertical = Input.GetAxis("Vertical"); //Gets the vertical change from the keyboard
+        Vector3 movement;
+        if (Input.GetKey(KeyCode.W)){
+            movement = new Vector3(0, 0, 1);
+            NULL = 1;
+        }
+        else if (Input.GetKey(KeyCode.S)){
+            movement = new Vector3(0, 0, -1);
+            NULL = 1;
+        }
+        else if (Input.GetKey(KeyCode.D)){
+            movement = new Vector3(1, 0, 0);
+            NULL = 1;
+        }
+        else if (Input.GetKey(KeyCode.A)){
+            movement = new Vector3(-1, 0, 0);
+            NULL = 1;
+        }
+        else if (Input.GetKey(KeyCode.Space))
+        {
+            movement = new Vector3(0, jump , 0);
+        }
+        else{
+            movement = new Vector3(0, 0, 0);
+            NULL = 0;
+        }
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical); //Vector3 needs to have three variable, xyz
-
+  
         rb.AddForce(movement * speed); //Changes where the player object is going based on the Vector3 variables
+        
     }
 }
