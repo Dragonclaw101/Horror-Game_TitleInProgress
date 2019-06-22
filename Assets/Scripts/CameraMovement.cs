723a4f0877966a05872cdced36ef25df65fe4137
Jo-Jo Hupp <huppjoesaphean@gmail.com>
2019-06-22 10:53:38 -0700

    Updated scripts 6/22/2019

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -6,26 +6,85 @@ public class CameraMovement : MonoBehaviour{
     public GameObject tempPlayer;
     public float speed;
     private Vector3 offset;
+    private Vector3 offsetY;
+
+    private float jump = 0.5f;
+    private float jumpcontrol = 0.25f;
+    private float jumpTime = 0f;
+
+    private float x;
+    private float Rx;
+
+    private float y;
+    private float Ry;
+
+    private float z;
+    private float Rz;
 
     void Start()
     {
         offset = transform.position - tempPlayer.transform.position;
+        offsetY = new Vector3(0, 4, 0);
         
+    }
+
+    void FixedUpdate(){
+        Camera mycam = GetComponent<Camera>();
+        if (mycam.transform.rotation.z != 180)
+        {
+            //mycam.transform.Rotate(mycam.transform.rotation.x, mycam.transform.rotation.y, 180);
+        }
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.position = transform.position + new Vector3(0, 0, speed);
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                transform.position = transform.position + new Vector3(0, 0, -speed);
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                transform.position = transform.position + new Vector3(speed, 0, 0);
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                transform.position = transform.position + new Vector3(-speed, 0, 0);
+            }
+            else if (Input.GetKey(KeyCode.Space))
+            {
+                if (transform.position.y <= 3)
+                    transform.position = transform.position + new Vector3(0, jump, 0);
+                transform.position = transform.position - new Vector3(0, jumpcontrol, 0);
+                jumpTime++;
+            }
+            else
+            {
+                jumpTime = 0;
+            }
+
+
+            //rb.AddForce(movement * (speed * NULL)); //Changes where the player object is going based on the Vector3 variables
+
+        }
     }
 
     void LateUpdate()
     {
-        //Camera mycam = GetComponent<Camera>();
-        transform.position = tempPlayer.transform.position + offset;
+        Camera mycam = GetComponent<Camera>();
+        //transform.position = tempPlayer.transform.position + offsetY;
         //transform.LookAt(mycam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, mycam.nearClipPlane)), Vector3.up);
         if (Input.GetKey(KeyCode.UpArrow))
-            transform.Rotate(-Vector3.right * speed * Time.deltaTime);
+            transform.Rotate(-Vector3.right * 45 * Time.deltaTime);
+
         else if (Input.GetKey(KeyCode.DownArrow))
-            transform.Rotate(Vector3.right * speed * Time.deltaTime);
+            transform.Rotate(Vector3.right * 45 * Time.deltaTime);
+
         else if (Input.GetKey(KeyCode.RightArrow))
-            transform.Rotate(Vector3.up * speed * Time.deltaTime);
+            transform.Rotate(Vector3.up * 45 * Time.deltaTime);
+
         else if (Input.GetKey(KeyCode.LeftArrow))
-            transform.Rotate(-Vector3.up * speed * Time.deltaTime);
+            transform.Rotate(-Vector3.up * 45 * Time.deltaTime);
 
        
     }
--- Assets/Scripts/Playercontroller.cs
+++ Assets/Scripts/Playercontroller.cs
@@ -4,44 +4,61 @@ using UnityEngine;
 
 public class Playercontroller : MonoBehaviour {
     private Rigidbody rb;
+    public GameObject Player;
     private float NULL = 1;
     public float speed; //Puts the variable in the editor to easily manipulate
-    public float jump;
+
+    private float jump = 0.5f;
+    private float jumpcontrol = 0.25f;
+    private float jumpTime = 0f;
     void Update() { }
 
     void Start(){ //Before anything is called, checks the player object for any compnents listed below
         rb = GetComponent<Rigidbody>();
     }
 
-    void FixedUpdate(){
+    void FixedUpdate()
+    {/*
         Vector3 movement;
-        if (Input.GetKey(KeyCode.W)){
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position = transform.position + new Vector3(0, 0, speed);
             movement = new Vector3(0, 0, 1);
-            NULL = 1;
+            NULL = 2;
         }
-        else if (Input.GetKey(KeyCode.S)){
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.position = transform.position + new Vector3(0, 0, -speed);
             movement = new Vector3(0, 0, -1);
             NULL = 1;
         }
-        else if (Input.GetKey(KeyCode.D)){
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = transform.position + new Vector3(speed, 0, 0);
             movement = new Vector3(1, 0, 0);
             NULL = 1;
         }
-        else if (Input.GetKey(KeyCode.A)){
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.position = transform.position + new Vector3(-speed, 0, 0);
             movement = new Vector3(-1, 0, 0);
             NULL = 1;
         }
         else if (Input.GetKey(KeyCode.Space))
         {
-            movement = new Vector3(0, jump , 0);
+            if (transform.position.y <= 3)
+                transform.position = transform.position + new Vector3(0, jump, 0);
+                transform.position = transform.position - new Vector3(0, jumpcontrol, 0);
+                jumpTime++;
         }
-        else{
-            movement = new Vector3(0, 0, 0);
-            NULL = 0;
+        else
+        {
+            jumpTime = 0;
         }
+        
 
   
-        rb.AddForce(movement * speed); //Changes where the player object is going based on the Vector3 variables
-        
+        //rb.AddForce(movement * (speed * NULL)); //Changes where the player object is going based on the Vector3 variables
+        */
     }
 }
